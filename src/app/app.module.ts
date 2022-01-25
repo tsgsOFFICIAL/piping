6b77f86907bf0b9411634bfc9dbb5d99916ee819
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-25 12:19:48 +0100

    created pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MorsePipe } from './pipes/morse.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MorsePipe,
+    FormsModule
   ],
   imports: [
     BrowserModule
